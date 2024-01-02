0216151f2855baf8fce5ca6a74bf32ca4116ea17
rajeshwari090 <134614230+rajeshwari090@users.noreply.github.com>
2024-01-02 20:12:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-grey': '#E8E9EA',
+        'custom-blue': '#2164E8',
+       
+        
+        
+      },
+
+    },
+  },
+  plugins: [],
+}
+
